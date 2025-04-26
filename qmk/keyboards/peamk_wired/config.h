08d932c836e81a1b81d80b0c87ae920d80bf806f
Ryan Adolf <ryanadolf123@gmail.com>
2025-04-26 16:22:25 -0400

    Switch to full duplex serial

--- qmk/keyboards/peamk_wired/config.h
+++ qmk/keyboards/peamk_wired/config.h
@@ -20,4 +20,10 @@
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 
+#define SPLIT_USB_DETECT
+#define SERIAL_USART_FULL_DUPLEX
+#define SERIAL_USART_PIN_SWAP
+#define SERIAL_USART_TX_PIN GP0
+#define SERIAL_USART_RX_PIN GP1
+
 #include "keyboards/peamk_wired/vik/config.vik.post.h"
